671408f3a31a1e671f9586c02f34029d7dff4640
KevinHubCo <kevincorreia2002@gmail.com>
2026-05-06 21:27:51 +0000

    Adicionei uma variavel int escolhaClient, adicione também um manu interativo para um "duelo" entre algum atributo, população, área, pib etc... e pra cada um desses atributos adicionei um duelo, de qual tiver o maior valor vence, apenas na densidade que coloquei o menor valor para vencer.

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -9,6 +9,8 @@
 int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
+    int escolhaClient;
+
      // Variaveis para a carta 1
     char estado1[3];
     char codigo1[5];
@@ -126,25 +128,94 @@ int main() {
     //SUPER PODER CARTA 2
     superpoder2 = (float)populacao2 + area2 + (float)pib2 + (float)turisticos2 + percapita2 + (1.0 / densidade2);
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+    //DESAFIO AVENTUREIRO ATUAL.
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+    printf("\n ======= ESCOLHA DE DUELO =======\n");
+    printf("Escolha um dos atributos: \n");
+    printf("1. População\n");
+    printf("2. Área\n");
+    printf("3. PIB\n");
+    printf("4. Número de pontos turísticos\n");
+    printf("5. Densidade demográfica\n");
+    printf("=================================\n");
+    printf("Digite sua escolha[1 - 5]: ");
+    scanf("%d", &escolhaClient);
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
-
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
-
-    printf("\n===== DUELO COMPARAÇÃO DE PIB =====\n");
+        switch (escolhaClient)
+    {
+    case 1:
+        printf("\n ======= DUELO DE POPULAÇÃO ======= \n");
+        printf("Carta 1 (%s): %lu habitantes\n", cidade1, populacao1);
+        printf("Carta 2 (%s): %lu habitantes\n", cidade2, populacao2);
+        if (populacao1 > populacao2) {
+        printf("Resultado: Carta 1 (%s) VENCEU!!!\n", cidade1, populacao1);
+    }else if (populacao2 > populacao1){
+        printf("Resultado: Carta 2 (%s) VENCEU!!!\n", cidade2, populacao2);
+    } else {
+        printf("Resultado: Empate! Ambas têm a mesma população %lu\n", populacao1);
+    }
+        break;
+    case 2:
+        printf("\n ======= DUELO DE ÁREA ======= \n");
+        printf("Carta 1 (%s): %.2fKm² de Área \n", cidade1, area1);
+        printf("Carta 2 (%s): %.2fKm² de Área \n", cidade2, area2);
+        if (area1 > area2) {
+        printf("Resultado: Carta 1 (%s) VENCEU!!!\n", cidade1, area1);
+    }else if (area2 > area1){
+        printf("Resultado: Carta 2 (%s) VENCEU!!!\n", cidade2, area2);
+    } else {
+        printf("Resultado: Empate!! Ambas têm a mesma área %.2fKm²\n", area1);
+    }
+        break;
+    case 3:
+        printf("\n ======= DUELO DE PIB ======= \n");
+        printf("Carta 1 (%s): R$%.1f de PIB \n", cidade1, pib1);
+        printf("Carta 2 (%s): R$%.1f PIB \n", cidade2, pib2);
+        if (pib1 > pib2) {
+        printf("Resultado: Carta 1 (%s) VENCEU!!!\n", cidade1, pib1);
+    }else if (pib2 > pib1){
+        printf("Resultado: Carta 2 (%s) VENCEU!!!\n", cidade2, pib2);
+    } else {
+        printf("Resultado: Empate! Ambas têm o mesmo pib R$%1.f\n", pib1);
+    }
+        break;
+    case 4:
+        printf("\n ======= DUELO DE PONTOS TURISTICOS ======= \n");
+        printf("Carta 1 (%s): %lu de Pontos Turisticos\n", cidade1, turisticos1);
+        printf("Carta 2 (%s): %lu de Pontos Turisticos \n", cidade2, turisticos2);
+        if (turisticos1 > turisticos2) {
+        printf("Resultado: Carta 1 (%s) VENCEU!!!\n", cidade1, turisticos1);
+    }else if (turisticos2 > turisticos1){
+        printf("Resultado: Carta 2 (%s) VENCEU!!!\n", cidade2, turisticos2);
+    } else {
+        printf("Resultado: Empate!! Ambas têm a mesma quantidade de pontos turisticos %lu\n", turisticos1);
+    }
+        break;
+case 5:
+        printf("\n ======= DUELO DE DENSIDADE DEMOGRÁFICA ======= \n");
+        printf("Carta 1 (%s): %.2f hab/Km²\n", cidade1, densidade1);
+        printf("Carta 2 (%s): %.2f hab/Km² \n", cidade2, densidade2);
+        if (densidade1 < densidade2) {
+        printf("Resultado: Carta 1 (%s) VENCEU!!!\n", cidade1, densidade1);
+    }else if (densidade2 < densidade1){
+        printf("Resultado: Carta 2 (%s) VENCEU!!!\n", cidade2, densidade2);
+    } else {
+        printf("Resultado: Empate!! Ambas têm a mesma densidade %2.f \n", densidade1);
+    }
+        break;
+    
+    
+    default:
+        printf("Opção Inválida! Escolha um número de 1 a 5.");
+        break;
+    }
+    
+    
+    
+    
+    
+    //DESAFIO ANTERIOR (NOVATO) DEIXEI COMENTADO
+    /*printf("\n===== DUELO COMPARAÇÃO DE PIB =====\n");
 
     //Mostrar os dados das duas cartas
     printf("Carta 1 - %s (%s): R$%.2f\n", cidade1, estado1, pib1);
@@ -158,7 +229,7 @@ int main() {
         printf("Resultado: Carta 2 (%s) venceu!\n", cidade2);
     } else {
         printf("Resultado: Empate entre %s e %s!\n", cidade1, cidade2);
-    }
+    }*/
 
     return 0;
 }
